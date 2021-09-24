8e3ac1a35cbfaf4eb5dbbbd2506ba768d99f2b37
Marko Milojevic <milojevicmarko92@gmail.com>
2021-09-24 08:41:09 +0200

    Add project files.

---
+++ 99Bottles/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace _99Bottles
+{
+    public static class StringExtensions
+    {
+        public static string Join(this IEnumerable<string> source, string separator) =>
+            string.Join(separator: separator, values: source);
+    }
+}
